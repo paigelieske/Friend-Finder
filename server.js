9ed2cedae024d7359a7edda8402dc02134747824
Paige Lieske <paigeproctor@comcast.net>
2019-05-06 12:00:31 -0400

    css link, pushing new data to api, showing routes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/app/assets"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
