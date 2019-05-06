b8233a81ddec69e673ccf67fc4319d3c6e67c36d
Paige Lieske <paigeproctor@comcast.net>
2019-05-06 14:54:07 -0400

    logic added but still no modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,14 +2,14 @@ var friends = [
     {
         "name": "Paige Lieske",
         "photo": "https://scontent.fatl1-1.fna.fbcdn.net/v/t1.0-9/1238813_1442873162604645_1968233429_n.jpg?_nc_cat=102&_nc_ht=scontent.fatl1-1.fna&oh=14d3bc70fbe7187d99698183cf871f47&oe=5D2C92D3",
-        "scores": [
+        "answers": [
             5, 2, 5, 2, 4, 5, 5, 3, 2, 2
         ]
     },
     {
         "name": "Taylor McGlothlin",
         "photo": "https://media.licdn.com/dms/image/C4D03AQHr3iq-8PptRQ/profile-displayphoto-shrink_800_800/0?e=1562803200&v=beta&t=qFHyyqR2EtQQStEDX4cuxFmB3xd9LeaIACg2VaoN1K8",
-        "scores": [
+        "answers": [
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1
         ]
     }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,26 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
+        console.log(req.body.answers);
+
+        var match = 0;
+        var minimum = 40;
+
+        for (var i = 0; i < friends.length; i++) {
+            var total = 0;
+            for (var j = 0; j < friends[i].answers.length; j++) {
+                var difference = Math.abs(req.body.answers[j] - friends[i].answers[j]);
+                total += difference;
+            }
+
+            if (total < minimum) {
+                match = i;
+                minimum = total;
+            }
+        }
+
         friends.push(req.body);
-        return res.json(true);
+
+        res.json(friends[match]);
     });
 };
